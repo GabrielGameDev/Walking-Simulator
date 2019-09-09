4c1dffe71ae47de89cf038bd88c9780fae739acf
Gabriel-PC\Gabriel <gabrielbarbosa@msn.com>
2019-09-09 10:54:13 -0300

    Verificando objetos interativos
    Verificação de objetos interativos. Cursor troca para outra imagem quando pode interagir com objeto

---
+++ Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+	public float rayDistance = 2f;
+
+	private Camera myCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		myCam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		CheckInteractables();
+    }
+
+	void CheckInteractables()
+	{
+		RaycastHit hit;
+		Vector3 rayOrigin = myCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.5f));
+
+		if(Physics.Raycast(rayOrigin, myCam.transform.forward, out hit, rayDistance))
+		{
+			Interactables interactable = hit.collider.GetComponent<Interactables>();
+			if(interactable != null)
+			{
+				UIManager.instance.SetHandCursor(true);
+			}
+			else
+			{
+				UIManager.instance.SetHandCursor(false);
+			}
+		}
+		else
+		{
+			UIManager.instance.SetHandCursor(false);
+		}
+
+	}
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+
+	public static UIManager instance;
+
+	public GameObject handCursor;
+
+	private void Awake()
+	{
+		instance = this;
+	}
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void SetHandCursor(bool state)
+	{
+		handCursor.SetActive(state);
+	}
+}
